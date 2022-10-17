4411a4ea8f94c4f4fc50b4a9c562fa6e4feafd53
deekshitha.a-at-411977029542 <deekshitha.a@juegostudioz.com>
2022-10-17 09:28:21 +0530

    FEAT:

--- src/staff-app/platform/activity.page.tsx
+++ src/staff-app/platform/activity.page.tsx
@@ -1,9 +1,30 @@
-import React from "react"
+import React, { useEffect } from "react"
 import styled from "styled-components"
 import { Spacing } from "shared/styles/styles"
+import { useApi } from "shared/hooks/use-api";
+import { CenteredContainer } from "shared/components/centered-container/centered-container.component";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Activity } from "shared/models/activity";
 
 export const ActivityPage: React.FC = () => {
-  return <S.Container>Activity Page</S.Container>
+
+  const [getStudents, data, loadState] = useApi<{ activity: Activity[] }>({
+    url: "get-activities",
+  });
+  useEffect(() => {
+    void getStudents();
+  }, [getStudents]);
+
+  useEffect(()=>{
+console.log(data?.activity)
+  },[data])
+  
+  return <S.Container> {loadState === "loading" && (
+          <CenteredContainer>
+            <FontAwesomeIcon icon="spinner" size="2x" spin />
+          </CenteredContainer>
+        )}
+</S.Container>
 }
 
 const S = {
